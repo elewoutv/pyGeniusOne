4088cf79443f8cbf78fdd2645ee5138ef2db39bb
Elewout Vercaeren <elewout.vercaeren@simac.be>
2020-03-17 13:57:44 +0100

    did some refactoring

--- pygeniusone.py
+++ pygeniusone.py
@@ -1,3 +1,9 @@
+##
+# Experimental python implementation of some of the netScout statistics calculations
+# Author: Elewout Vercaeren
+# Released under GPL-3.0
+#
+
 import argparse
 
 # Calculation parameter defaults
@@ -5,87 +11,88 @@ RESOLUTION_TIME_DEFAULT = 1000
 SILENCE_PERIOD_DEFAULT = 4
 MINIMUM_REPORT_TIME_DEFAULT = 1
 
-parser = argparse.ArgumentParser(description='Calculate netScout subscriber session statistics')
 
-# Optional arguments
-parser.add_argument('-f',
-                    help="file to write output to")
-parser.add_argument('--resolution-time',
-                    type=int,
-                    default=RESOLUTION_TIME_DEFAULT,
-                    dest='resolutionTime',
-                    help="resolution time in ms")
-parser.add_argument('--silence-period',
-                    type=int,
-                    default=SILENCE_PERIOD_DEFAULT,
-                    dest='silencePeriod',
-                    help="silence period time in s")
-parser.add_argument('--minimum-report-time',
-                    type=int,
-                    default=MINIMUM_REPORT_TIME_DEFAULT,
-                    dest='minReportTime',
-                    help="minimum report time in ms")
-parser.add_argument('-u', '--up',
-                    help="calculate only upload stats",
-                    default=False,
-                    dest='up',
-                    action="store_true")
-parser.add_argument('-d', '--down',
-                    help="calculate only download stats",
-                    default=False,
-                    dest='down',
-                    action="store_true")
+def main():
 
-# Required arguments
-parser.add_argument('filename.pcap',
-                    help="subscriber session pcap file")
+    parser = argparse.ArgumentParser(description='Calculate netScout subscriber session statistics')
+    # Optional arguments
+    parser.add_argument('-f',
+                        help="file to write output to")
+    parser.add_argument('--resolution-time',
+                        type=int,
+                        default=RESOLUTION_TIME_DEFAULT,
+                        dest='resolution_time',
+                        help="resolution time in ms")
+    parser.add_argument('--silence-period',
+                        type=int,
+                        default=SILENCE_PERIOD_DEFAULT,
+                        dest='silence_period',
+                        help="silence period time in s")
+    parser.add_argument('--minimum-report-time',
+                        type=int,
+                        default=MINIMUM_REPORT_TIME_DEFAULT,
+                        dest='min_report_time',
+                        help="minimum report time in ms")
+    parser.add_argument('-u', '--up',
+                        help="calculate only upload stats",
+                        default=False,
+                        dest='up',
+                        action="store_true")
+    parser.add_argument('-d', '--down',
+                        help="calculate only download stats",
+                        default=False,
+                        dest='down',
+                        action="store_true")
+    # Required arguments
+    parser.add_argument('filename.pcap',
+                        help="subscriber session pcap file")
+    required_group = parser.add_argument_group(title="required arguments")
+    required_group.add_argument('-i', '--ip-adress',
+                                help="ip adress of the server (the receiver)",
+                                dest='ip',
+                                required=True)
+    # Output arguments
+    output_group = parser.add_argument_group(title="output options",
+                                             description="pyGeniousOne will output all available statistics by default,"
+                                             " you can limit the output with these options")
+    output_group.add_argument('-C', '--count-packets',
+                              help="Include packet count in output",
+                              default=False,
+                              dest='packetCount',
+                              action="store_true")
+    output_group.add_argument('-c', '--count-bytes',
+                              help="Include total byte count in output",
+                              default=False,
+                              dest='bytes',
+                              action="store_true")
+    output_group.add_argument('-e', '--effective-bytes',
+                              help="Include GTP-U byte count in output",
+                              default=False,
+                              dest='effectieBytes',
+                              action="store_true")
+    output_group.add_argument('-a', '--active-millis',
+                              help="Include total transmission time in a specific direction in output",
+                              default=False,
+                              dest='activeMillis',
+                              action="store_true")
+    output_group.add_argument('-t', '--max-throughput',
+                              help="Include highest throughput reached in specific direction in output",
+                              default=False,
+                              dest='maxThroughput',
+                              action="store_true")
+    args = parser.parse_args()
 
-requiredGroup = parser.add_argument_group(title="required arguments")
+    # Get silence period
+    silence_period = args.silencePeriod
 
-requiredGroup.add_argument('-i', '--ip-adress',
-                           help="ip adress of the server (the receiver)",
-                           dest='ip',
-                           required=True)
+    # Get minimum report time
+    min_report_time = args.minReportTime
 
-# Output arguments
-outputGroup = parser.add_argument_group(title="output options",
-                                        description="pyGeniousOne will output all available statistics by default,"
-                                                    " you can limit the output with these options")
-outputGroup.add_argument('-C', '--count-packets',
-                         help="Include packet count in output",
-                         default=False,
-                         dest='packetCount',
-                         action="store_true")
-outputGroup.add_argument('-c', '--count-bytes',
-                         help="Include total byte count in output",
-                         default=False,
-                         dest='bytes',
-                         action="store_true")
-outputGroup.add_argument('-e', '--effective-bytes',
-                         help="Include GTP-U byte count in output",
-                         default=False,
-                         dest='effectieBytes',
-                         action="store_true")
-outputGroup.add_argument('-a', '--active-millis',
-                         help="Include total transmission time in a specific direction in output",
-                         default=False,
-                         dest='activeMillis',
-                         action="store_true")
-outputGroup.add_argument('-t', '--max-throughput',
-                         help="Include highest throughput reached in specific direction in output",
-                         default=False,
-                         dest='maxThroughput',
-                         action="store_true")
-args = parser.parse_args()
+    # Get resolution time
+    resolution_time = args.resolutionTime
 
-# Set silence period
-SILENCE_PERIOD = args.silencePeriod
 
-# Set minimum report time
-MINIMUM_REPORT_TIME = args.minReportTime
-
-# Set resolution time
-RESOLUTION_TIME = args.resolutionTime
+main()
 
 # Set direction to calculate
 # Set receiver IP
