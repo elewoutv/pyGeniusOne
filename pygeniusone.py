2fb01c18decbd1d28da92d7939bfa2ce254331c9
Elewout Vercaeren <elewout.vercaeren@simac.be>
2020-03-23 15:24:46 +0100

    implemented userplane_[download/upload]_packets_count

--- pygeniusone.py
+++ pygeniusone.py
@@ -4,9 +4,11 @@
 # Released under GPL-3.0
 #
 
+import json
 import argparse
 import sys
 import helpers
+from scapy.all import *
 
 
 def main():
@@ -39,6 +41,30 @@ def main():
 
     # The netscout calculations expect chunks of 5 minutes so we subdivide the pcap in chunks of 5 minutes
     pcap_chunks = helpers.divide_in_chunks(pcap, 300.00)
+
+    # amount of packets in upload/download direction in the session
+    upload_count = 0
+    download_count = 0
+
+    # we need this to print the chunk number between the JSON's
+    chunk_count = 0
+
+    # We scan all packets and match the subscriber_ip from commandline to the src ip of the outer IP layer.
+    # If we find a match we count it as upload, all other packets are counted as download
+
+    for chunk in pcap_chunks:
+        for pkt in chunk:
+            if pkt["IP"].src == subscriber_ip:
+                upload_count += 1
+            else:
+                download_count += 1
+        data = {'userplane_upload_packets_count': upload_count, 'userplane_download_packets_count': download_count}
+        print("time chunk", chunk_count)
+        json_data = json.dumps(data)
+        print(json_data)
+        chunk_count += 1
+        upload_count = 0
+        download_count = 0
 
 
 main()
